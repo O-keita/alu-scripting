64093bc4d08bc2bfd0a51dfc4bdc91c305b211d8
O-keita <o.keita@alustudent.com>
2024-02-27 19:32:54 +0200

    sadd

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,26 @@
-#!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
+def get_subreddit_subscribers(subreddit):
+    """
+    Return the number of subscribers for the given subreddit.
 
-def number_of_subscribers(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    Parameters:
+    - subreddit (str): The name of the subreddit.
 
+    Returns:
+    - int: The number of subscribers, or 0 if the subreddit is not valid or there's an issue with the request.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
+        if response.status_code == 200:
+            data = response.json().get('data')
+            if data:
+                return data.get('subscribers', 0)
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+
     return 0
-
-"""
-0-main
-"""
-import sys
-
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
