286c0560d12f379c57d09612678536adf63380da
O-keita <o.keita@alustudent.com>
2024-02-27 19:03:14 +0200

    let's try

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""Api is getting over my head"""
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function getting the API"""
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    headers = {
-        "User-Agent":"getting number of subs"
-    }
-
+    """Return the number of subscribers for the given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code ==200:
-        data = response.json()
-
-        subs = data.get('data').get('subscribers')
-
-        return subs
-    else:
-        return 0
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+    return 0
