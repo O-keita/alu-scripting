bca727b23533abf5a3e7a5f07ba19b0b8b253425
O-keita <o.keita@alustudent.com>
2024-02-27 22:17:55 +0200

    check

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,18 +7,24 @@ def top_ten(subreddit):
     """Return number of subscribers if @subreddit is valid subreddit.
     if not return 0."""
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    headers = {'User-Agent': 'just checking'}
     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    response = requests.get(subreddit_url, headers=headers, params={'limit':10})
 
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+
+        data = response.json().get('data').get('children')
+
+        for single_data in data:
+
+            title = single_data.json().get('data').get('title')
+
+            print(title)
+
+        return title        
+            
     else:
-        print(None)
+        return_value =None
+
+        return return_value
+
